b35acf0cb3819648d2307d86fb2d164f34d30b51
Nwafor Ugochukwu Emmanuel <34918992+Emmyglobal@users.noreply.github.com>
2025-07-15 12:30:45 +0100

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,9 +6,9 @@ export default function AboutPage() {
   return (
     <div className="max-w-4xl mx-auto px-6 py-12 text-gray-800 dark:text-gray-100">
         <Header />
-      <h1 className="text-4xl font-bold mb-6">About Digital Dose</h1>
+      <h1 className="text-4xl font-bold mb-6">About BOLDATLAS</h1>
       <p className="text-lg mb-4">
-        Welcome to <strong>Digital Dose</strong> – your daily source for insightful stories and breakthroughs across Africa and the world. We&apos;re passionate about exploring how technology, culture, health, and innovation shape the modern world.
+        Welcome to <strong>BOLDATLAS</strong> – your daily source for insightful stories and breakthroughs across Africa and the world. We&apos;re passionate about exploring how technology, culture, health, and innovation shape the modern world.
       </p>
       <p className="text-lg mb-4">
         Our mission is to shed light on transformative ideas and real-world impact. Whether it’s climate adaptation in African cities, AI trends, or healthcare breakthroughs, our blog provides curated content to inspire, educate, and empower.
